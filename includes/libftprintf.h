43e58ccf18c04b92177517f51e50955d56546902
Jibran Kalia <jkalia@e1z1r2p14.42.us.org>
2017-03-23 15:48:23 -0700

    Changed prototype in .h from fmt to in

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 14:07:51 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/23 15:01:09 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/23 15:47:47 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 # include <stdint.h>
 # include <libft.h>
 
-int	ft_printf(const char *fmt, ...);
+int	ft_printf(const char *in, ...);
 
 #endif
