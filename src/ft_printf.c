0f33976d2f87400600c7d7937ff96aaff982c62d
Jibran Kalia <jkalia@e1z1r2p14.42.us.org>
2017-03-23 20:56:28 -0700

    Initial work

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 14:38:22 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/23 15:01:31 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/23 20:55:38 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,31 @@
 
 int		ft_printf(const char *fmt, ...)
 {
-	ft_putstr(fmt);
-	return (0);
+	va_list		ap;
+	char		*ret;
+	int			done;
+
+	if (fmt == 0 || *fmt == 0)
+		return (0);
+
+	va_start (ap, fmt);
+	done = ft_vasprintf(&ret, fmt, ap); // ADD CHK
+	write(1, ret, done); //ADD CHK
+	free(ret);
+	va_end (ap);
+	return (done);
+}
+
+int		ft_vasprintf(char **ret, const char *fmt, va_list ap)
+{
+
+	if (fmt == 0 || *fmt == 0 || ret == 0)
+		return (0);
+	*ret = 0;
+
+	if (ft_strchr(*fmt, '%') == 0)
+	{
+		// DUP onto ret.
+	}
+	r = dispatch(
 }
