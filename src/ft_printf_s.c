4bcff6b480de39d42fdf332db8aeec0c21cf0416
Jibran Kalia <jkalia@e1z2r7p10.42.us.org>
2017-04-05 16:33:20 -0700

    Normal string completed

--- src/ft_printf_s.c
+++ src/ft_printf_s.c
@@ -6,25 +6,32 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/04 13:53:24 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/04 17:55:48 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/05 16:23:56 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libftprintf.h>
 
+int8_t		ft_printf_wstr(t_arr *ret, const char **fmt, t_printf *x, va_list clone)
+{
+	wchar_t *tmp;
+
+	tmp = va_arg(clone, wchar_t*);
+}
 int8_t			ft_printf_s(t_arr *ret, const char **fmt, t_printf *x, va_list clone)
 {
 	char	*tmp;
+	char	*final;
 	int		n;
+	int		len;
 
-	CHK1((ft_arr_init(&x->extra, 1)) == -1, ft_arr_del(ret), -1);
+
+	CHK1((ft_arr_init(&x->extra, 5)) == -1, ft_arr_del(ret), -1);
 	tmp = va_arg(clone, char*);
-	if (tmp == 0)
-		tmp = "(null)";
-	n = (x->is_prec == 1) ? ft_abs(x->prec) : ft_strlen(tmp);
-	ft_arr_appendn(&x->extra, tmp, n * sizeof(char));
-//	printf("Seg?\n");
-	//printf("Before %d\n", &x->extra.len);
+	final = (tmp != NULL) ? ft_strdup(tmp) : ft_strdup("(null)");
+	n = (x->is_prec == 1) ? MIN(x->prec, ft_strlen(final)) : ft_strlen(final);
+	ft_arr_appendn(&x->extra, final, sizeof(char) * n);
+	free(final);
 	ft_handlewidth(x);
 	return (ft_printf_append(ret, fmt, x));
 }
