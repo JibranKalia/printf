f24b0230623dea0b1ac07c961bb7f5eef0059dd7
Jibran Kalia <jkalia@e1z2r7p10.42.us.org>
2017-04-05 23:47:20 -0700

    Normed libftprintf.h

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -6,111 +6,70 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 14:07:51 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/05 21:15:23 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/05 23:47:00 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_PRINTF_H
-# define LIBFT_PRINTF_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 # include <unistd.h>
 # include <stdarg.h>
 # include <stdlib.h>
 # include <limits.h>
 # include <stdint.h>
-# include <wchar.h> //needed for wchar_t in %c
-
+# include <wchar.h>
 # include "libft.h"
+# include "array.h"
 # include "test.h"
 
-typedef struct		s_arr
-{
-	char			*ptr;
-	size_t			len;
-	size_t			cap;
-}					t_arr;
-
-typedef struct		s_printf
-{
-	t_arr			extra;
-	unsigned int 	is_prec:1;       /* Whether precision is a factor or not. */
-	int 			prec;            /* Precision. Default is -1??? */
-	int 			width;           /* Width. Default is 0 */
-	wchar_t 		spec;            /* Format letter.  */ //NOT SURE IF KEEPING
-	unsigned int 	left:1;          /* - flag.  */
-	unsigned int 	showsign:1;      /* + flag.  */
-	unsigned int 	space:1;         /* Space flag.  */
-	unsigned int 	zero:1;          /* Zero flag.  */
-	unsigned int 	alt:1;           /* # flag.  */
-	uint8_t			len_mod;         /* Change below */
-	char	 		pad;             /* Padding character.  The value is '0' if the ‘0’ flag was specified, and ' ' otherwise.*/
-
-}					t_printf;
-
-
-int					ft_printf(const char *in, ...);
-int					ft_vasprintf(char **ret, const char *fmt, va_list ap);
-int					ft_asprintf(char **ret, const char *fmt, ...);
-
-
-/*
-** Arrays
-*/
-
-int8_t				ft_arr_init(t_arr *src, size_t cap);
-int8_t				ft_arr_append(t_arr *dst, const void *src);
-int8_t				ft_arr_appendn(t_arr *dst, const void *src, size_t n);
-int8_t				ft_arr_append_arr(t_arr *dst, t_arr *src);
-int8_t				ft_arr_resize(t_arr *src, size_t sze);
-int8_t				ft_arr_insert(t_arr *dst, size_t index, const void *src);
-int8_t				ft_arr_insertn(t_arr *dst, size_t index, const void *src, size_t src_len);
-void				ft_arr_del(t_arr *src);
-char				*ft_arrtostr(t_arr *src);
+int		ft_printf(const char *in, ...);
+int		ft_vasprintf(char **ret, const char *fmt, va_list ap);
+int		ft_asprintf(char **ret, const char *fmt, ...);
 
 /*
 ** Flags
 */
 
-int8_t				ft_printf_flags(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_handlewidth(t_printf *x);
+int8_t	ft_printf_flags(t_arr *r, const char **f, t_printf *x, va_list clone);
+int8_t	ft_handlewidth(t_printf *x);
 
 /*
 ** Width
 */
 
 # define ISWIDTH(a) (a > 48 && a <= 57)
-int8_t				ft_printf_width(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+
+int8_t	ft_printf_width(t_arr *r, const char **f, t_printf *x, va_list clone);
 
 /*
 ** Precision
 */
 
-int8_t				ft_printf_dot(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_dot(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
 
 /*
 ** Length
 */
 
-int8_t				ft_printf_length(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_length(t_arr *r, const char **f, t_printf *x, va_list clone);
 
 /*
 ** Functions
 */
 
-int8_t				handle_prec(t_printf *x, intmax_t org);
-
-int8_t				ft_printf_init(t_printf *x);
-int8_t				ft_printf_c(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_C(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_d(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_percent(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_p(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_s(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_S(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_o(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_u(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_X(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
-int8_t				ft_printf_append(t_arr *ret, const char **fmt, t_printf *x);
-int					dispatch(char **final, const char *fmt, va_list clone);
-
+int8_t	handle_prec(t_printf *x, intmax_t org);
+int8_t	ft_printf_init(t_printf *x);
+int8_t	ft_printf_c(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_c1(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_d(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_percent(t_arr *r, const char **f, t_printf *x, va_list cln);
+int8_t	ft_printf_p(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_s(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_s1(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_o(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_u(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_x1(t_arr *ret, const char **fmt, t_printf *x, va_list clone);
+int8_t	ft_printf_append(t_arr *ret, const char **fmt, t_printf *x);
+int		dispatch(char **final, const char *fmt, va_list clone);
 #endif
