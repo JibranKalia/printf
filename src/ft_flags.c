924ea2238fea28a4fbaabcc5979833aa6238347b
Jibran Kalia <jkalia@e1z2r7p10.42.us.org>
2017-04-10 13:00:12 -0700

    Almost Normed ft_flags

--- src/ft_flags.c
+++ src/ft_flags.c
@@ -6,62 +6,68 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/27 20:42:59 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/06 15:37:45 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/10 12:59:34 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libftprintf.h>
-# define ISSIGN(a) (a == '+' || a == '-')
+#define ISSIGN(a) (a == '+' || a == '-')
+#define ISFLAG(a) (a == '+' || a == '-' || a == ' ' || a == '#' || a == '0')
 
-int8_t	ft_printf_flags(t_arr *ret, const char **fmt, t_printf *x, va_list clone)			//What should this be returning?
+/*
+** Flag '0' is ignored if flag '-' is present.
+** flag ' ' is ignored if '+' is present.
+*/
+
+int8_t	ft_printf_flags(t_arr *ret, const char **fmt,
+		t_printf *x, va_list clone)
 {
 	(void)ret;
 	(void)clone;
-	while (*fmt && (**fmt == '-' || **fmt == '+' || **fmt == ' ' || **fmt == '#' || **fmt == '0'))
+	while (*fmt && ISFLAG(**fmt))
 	{
 		if (**fmt == '-')
 		{
-//			printf("Negative Flag\n");
 			x->left = 1;
-			x->zero = 0;	//flag '0' is ignored when flag '-' is present
+			x->zero = 0;
 		}
 		else if (**fmt == '+')
 		{
-//			printf("Positive Flag\n");
 			x->showsign = 1;
-			x->space = 0; //flag ' ' is ignored when flag '+' is present
+			x->space = 0;
 		}
 		else if (**fmt == ' ')
 		{
-			if (x->showsign == 0)  //flag ' ' is ignored when flag '+' is present
+			if (x->showsign == 0)
 				x->space = 1;
-//			printf("Space Flag\n");
 		}
 		else if (**fmt == '#')
 		{
-//			printf("Hash Flag\n");
 			x->alt = 1;
 		}
 		else if (**fmt == '0')
 		{
-//			printf("Zero Flag\n");
-			if (x -> left == 0) /** flag '0' is ignored if - flag is present.**/
+			if (x->left == 0)
 				x->zero = 1;
 		}
 		++*fmt;
 		++x->extra.len;
 	}
-	x->pad = (x->zero) ? '0' : ' '; //Doing at the end after all the flags are handled
+	x->pad = (x->zero) ? '0' : ' ';
 	return (0);
 }
 
-int8_t		handle_width(t_printf *x, char c)
+/*
+** If prec is set. '0' is ignored.
+*/
+
+int8_t	handle_width(t_printf *x, char c)
 {
 	char	*tmp;
 	int		diff;
 	size_t	index;
 
-	if (x->is_prec == 1)                     //Make sure pad is space if precision is set. (This doesn't include default precision);
+	if (x->is_prec == 1)
 		x->pad = ' ';
 	diff = x->width - x->extra.len;
 	index = (x->left == 1) ? x->extra.len : 0;
@@ -79,42 +85,44 @@ int8_t		handle_width(t_printf *x, char c)
 	return (0);
 }
 
-int8_t				ft_printf_width(t_arr *ret, const char **fmt, t_printf *x, va_list clone)
+/*
+** Width takes the value of the last asterisk if there are multiple.
+** If width is negative. Set '-' flag.
+*/
+
+int8_t	ft_printf_width(t_arr *ret, const char **fmt,
+		t_printf *x, va_list clone)
 {
 	size_t	res;
 
 	res = 0;
 	if (**fmt == '0')
-		return (ft_printf_flags(ret, fmt, x, clone)); //Starting with zero is a flag);
+		return (ft_printf_flags(ret, fmt, x, clone));
 	while (ISDIGIT(**fmt))
 	{
 		res = res * 10 + (**fmt - '0');
 		++*fmt;
 	}
 	x->width = res;
-	//Width takes the value of the last asterisk if they are in the a row.
 	while (**fmt == '*')
 	{
 		x->width = va_arg(clone, int);
-//		printf("Width = %d\n", x->width);
 		++*fmt;
 	}
-	//If width is somehow negative. The - is treated as a flag.
 	if (x->width < 0)
 	{
-			x->width = -x->width;
-			x->left = 1;
+		x->width = -x->width;
+		x->left = 1;
 	}
-//	printf("Width = %d\n", x->width);
 	return (0);
 }
 
 int8_t	ft_printf_dot(t_arr *ret, const char **fmt, t_printf *x, va_list clone)
 {
-	(void)ret;
-	(void)clone;
 	int res;
 
+	(void)ret;
+	(void)clone;
 	res = 0;
 	++*fmt;
 	x->is_prec = 1;
@@ -132,7 +140,6 @@ int8_t	ft_printf_dot(t_arr *ret, const char **fmt, t_printf *x, va_list clone)
 		}
 	}
 	x->prec = res;
-//	printf("Precision = %d\n", x->prec);
 	return (0);
 }
 
@@ -147,15 +154,15 @@ int8_t	ft_printf_dot(t_arr *ret, const char **fmt, t_printf *x, va_list clone)
 */
 
 char	g_length_spec[7] = {"hhlljzL"};
+#define ISLENSPEC(a) (a == 'h' || a == 'l' || a == 'j' || a == 'z' || a == 'L')
 
-#define ISLENSPEC(a) ((a == 'h') || (a == 'l') || (a == 'j') || (a == 'z') || (a == 'L'))
-
-int8_t	ft_printf_length(t_arr *ret, const char **fmt, t_printf *x, va_list clone)
+int8_t	ft_printf_length(t_arr *ret, const char **fmt,
+		t_printf *x, va_list clone)
 {
-	(void)ret;
-	(void)clone;
 	int		index;
 
+	(void)ret;
+	(void)clone;
 	while (ISLENSPEC(**fmt))
 	{
 		index = ft_strchr(g_length_spec, **fmt) - g_length_spec + 1;
@@ -168,11 +175,10 @@ int8_t	ft_printf_length(t_arr *ret, const char **fmt, t_printf *x, va_list clone
 		}
 		if (index == 3)
 		{
-			index = ((*(*fmt + 1)) == 'l') ? index + 1 : index;   //If it is 'll' index is incremented to 4
+			index = ((*(*fmt + 1)) == 'l') ? index + 1 : index;
 		}
 		x->len_mod = (index > x->len_mod) ? index : x->len_mod;
 		++*fmt;
-//		printf("Len Mod = %d\n", x->len_mod);
 	}
 	return (0);
 }
