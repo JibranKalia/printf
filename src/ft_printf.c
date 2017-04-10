b0487739ca66f9d2161470e6d5fc217a9d57eb4d
Jibran Kalia <jkalia@e1z2r7p10.42.us.org>
2017-04-10 12:47:45 -0700

    Normed ft_printf.c

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 14:38:22 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/06 14:11:34 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/10 12:47:26 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ int		ft_printf(const char *fmt, ...)
 	char		*ret;
 	int			len;
 
+	va_start(ap, fmt);
 	if (fmt == 0 || *fmt == 0)
 		return (0);
-
-	va_start (ap, fmt);
 	CHK((len = ft_vasprintf(&ret, fmt, ap)) == -1, -1);
 	write(1, ret, len);
 	free(ret);
-	va_end (ap);
+	va_end(ap);
 	return (len);
 }
 
@@ -34,12 +33,11 @@ int		ft_asprintf(char **ret, const char *fmt, ...)
 	va_list		ap;
 	int			len;
 
+	va_start(ap, fmt);
 	if (fmt == 0 || *fmt == 0)
 		return (0);
-
-	va_start (ap, fmt);
-	len = ft_vasprintf(ret, fmt, ap); // ADD CHK
-	va_end (ap);
+	CHK((len = ft_vasprintf(ret, fmt, ap)) == -1, -1);
+	va_end(ap);
 	return (len);
 }
 
@@ -51,16 +49,13 @@ int		ft_vasprintf(char **ret, const char *fmt, va_list ap)
 	if (fmt == 0 || *fmt == 0 || ret == 0)
 		return (0);
 	*ret = 0;
-	if (ft_strchr(fmt, '%') == NULL) // % Not found. Print as it is.
+	if (ft_strchr(fmt, '%') == NULL)
 	{
 		*ret = ft_strdup(fmt);
 		return (ft_strlen(fmt));
 	}
-	va_copy(clone, ap); //Why clone again?
-	len = dispatch(ret, fmt, clone); // VAS Prints out on to the malloc'd string.
+	va_copy(clone, ap);
+	len = dispatch(ret, fmt, clone);
 	va_end(clone);
 	return (len);
 }
-//TODO: Need to set pointer to null if return is -1;
-
-// there is another way to do it, parse the whole fmt string and va_arg to calculate the size of the malloc.
