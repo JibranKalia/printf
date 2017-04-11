ad36fc4c4b2560c9c75d52f826437e7f5357e659
Jibran Kalia <jkalia@e1z2r9p7.42.us.org>
2017-04-10 18:57:19 -0700

    Fixed small issues with p

--- src/ft_printf_dp.c
+++ src/ft_printf_dp.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 17:00:15 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/10 17:14:28 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/10 18:53:58 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,16 +66,11 @@ int8_t		ft_printf_p(t_arr *ret, const char **fmt,
 	char		*nbr;
 
 	CHK1((ft_arr_init(&x->extra, 5)) == -1, ft_arr_del(ret), -1);
-	ft_arr_insertn(&x->extra, 0, "0x", 2);
 	org = (uintmax_t)va_arg(clone, void*);
-	if (org == 0)
-	{
-		handle_width(x, 'p');
-		return (ft_printf_append(ret, fmt, x));
-	}
-	nbr = ft_itoa_base(org, 16, "0123456789abcdef");
+	nbr = (org != 0) ? ft_itoa_base(org, 16, "0123456789abcdef") : ft_strdup("0");
 	ft_arr_appendn(&x->extra, nbr, sizeof(char) * ft_strlen(nbr));
 	handle_prec(x, org);
+	ft_arr_insertn(&x->extra, 0, "0x", 2);
 	free(nbr);
 	handle_width(x, 'p');
 	return (ft_printf_append(ret, fmt, x));
